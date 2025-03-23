b268adeaf950c2d3514b0a6943df7787f9dc1559
asmit-jha <asmitjha0812@gmail.com>
2025-03-23 17:31:15 +0530

    Updated to version 0.2.0 with new features/fixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 # Setup function
 setup(
     name='image_capturer',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=['opencv-python'],
     author='Asmit Jha',
